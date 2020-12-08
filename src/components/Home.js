3493daeca56b75655168871a1b51fe30b29e858a
arnabuchiha <arnabkeya04@gmail.com>
2020-12-08 21:40:33 +0530

    Project init

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import React from "react"
+
+class Home extends Component {
+  render(){
+    return(
+      <div className="">
+        adsda
+      </div>
+    )
+  }
+}
+
+export default Home;
---
+++ src/components/Nav.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {BrowserRouter as Router,Link} from 'react-router-dom';
+
+class Nav extends React.Component {
+  render() {    
+    return (
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light navbar-custom">
+            <a className="navbar-brand" href="#">
+            <img src="" width="auto" height="30" className="d-inline-block align-top" alt=""/>
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" id="navbarDropdown"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">About</a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="nav-link dropdown-item" href="/about">Team</a>
+                        <a className="nav-link dropdown-item" href="/blog">Blog</a>
+                        <a className="nav-link dropdown-item" href="/faq">FAQ</a>
+                    </div>
+                </li>
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" id="navbarDropdown"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">Services</a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="nav-link dropdown-item" href="/service">Audit Solutions</a>
+                        <a className="nav-link dropdown-item" href="/telecomAuditOptimization">Telecom Audit and Optimization</a>
+                        <a className="nav-link dropdown-item" href="/mobileWirelessAudit">Mobile and Wireless Audit</a>
+                        <a className="nav-link dropdown-item" href="/telecomRefundAudit">Telecom Refund Audit</a>
+                        <a className="nav-link dropdown-item" href="/telecomTaxAudit">Telecom Tax Audit</a>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/contact">Contact</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/career">Career</a>
+                </li>
+                <li className="nav-item">
+                <a href="/login" style={{textDecoration:"none", color:"white"}}><button className="btn my-2 my-sm-0 get-started-button">Get Started</button></a>
+                </li>
+                </ul>
+            </div>
+      </nav>
+    );
+  }
+}
+export default Nav;
