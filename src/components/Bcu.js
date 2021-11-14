5dfef6c5921dfdd105cc8f02f65ca5db5fcdf3bb
arnabuchiha <arnabkeya04@gmail.com>
2021-11-14 12:28:32 +0530

    Bio composting replace

--- src/components/Bcu.js
+++ src/components/Bcu.js
@@ -1,51 +1,253 @@
 import React from "react";
-import Slider from './Slider/Slider';
+import Slider from "./Slider/Slider";
 import slide1 from "../assets/images/home_slide1.jpg";
 import slide2 from "../assets/images/home_slide2.jpg";
 import slide3 from "../assets/images/home_slide3.jpg";
-import bcu1 from "../assets/images/composting.png"
+import bcu1 from "../assets/images/composting.png";
+import "../assets/styles/aboutus.css";
 
-class Bcu extends React.Component{
-    render(){
-        return(
-            <div className="Bcu">
-                
-
-                <div className="container-fluid" style={{backgroundColor:"rgb(245, 245, 245)"}}>
-                    <div className="subheading pt-5">
-                        <h2>BIO-COMPOSTING UNITS</h2>
-                    </div>
-
-                    <div className="container">
-                        <div className="row pt-5 pb-5">
-                            <div className="col-lg-8" style={{textAlign:"left"}}>
-                                <p>Bio-Composting is the process of breaking down the organic matter (vegetable and food waste)
-                                     in the presence or absence of air, using microorganisms and small insects present in nature.
-                                      The end product is called compost which is rich in readily usable plant nutrients forming
-                                       a part of healthy soil. When applied to soil, this compost provides nutrients to soil.</p>
-
-                                <p>Bio-Compost increases agricultural productivity, improved soil biodiversity, reduced ecological
-                                     risks and a better environment. It destroys pathogens and reduces the volume of waste.</p>
-
-                                <p>Bio- Compost can be easily made at Home/ Restaurants/ Canteens from daily waste vegetables & Fruits.</p>
-                                <p>The COMPOSTING UNIT requires a COMPOST MAKER & COMPOST ADDITIVE for its functioning.</p>
-                                <p>ach UNIT includes a Facilitator Guide which contains overview, materials list, and preparation 
-                                    steps, followed by clear, step-by-step instructions for effective hands on application.</p>
-
-                                <p>***** ORDERS Can be Given by MAIL OR PHONE</p>
-                            </div>
-
-                            <div className="col-lg-4">
-                                <img src={bcu1} className="img-fluid" alt="/"/>
-                            </div>
-                        </div>
-
-                        <div></div>
-                    </div>
-                </div>
+class Bcu extends React.Component {
+  render() {
+    return (
+      <div className="Aboutus">
+        <div className="container-fluid" style={{ backgroundColor: "#F5F5F5" }}>
+          <h2 className="heading pt-5">Projects for Training ( ONLINE)</h2>
+          <br />
+          <div className="container">
+            <div className="row mt-5">
+              <div className="col-lg-8" style={{ textAlign: "left" }}>
+                <h6>
+                  List of Broad Subjects in Bioinformatics for project based
+                  Internship.
+                </h6>
+                <ul>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Protein homology modeling.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Sequence analysis for human disease targets.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Structure analysis for human disease targets.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Phylogenetic analysis for human disease targets.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Study and In- silico analysis of variable region of human
+                    immunoglobulin.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Compositional and structural analysis of human Immunoglobin
+                    related to tumors or viral diseases
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Molecular docking studies and repurposing drug
+                    identification of various protein targets and drugs ( For
+                    different diseases and cancer)
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Molecular modelling studies of various drugs
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Analysis of target molecule for disease & identifying drug
+                    candidate by using in silico tools.
+                  </li>
+                </ul>
+              </div>
             </div>
-        )
-    }
+            <br />
+            <br />
+            {/*end of row1*/}
+
+            <div className="row pt-3">
+              <div className="col-lg-8" style={{ textAlign: "left" }}>
+                <h6>
+                  List of Specific Topics in Bioinformatics related projects
+                </h6>
+                <ul>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Identification of Potential drug targets for Burkholderia
+                    sp. Using metabolic pathway analysis.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Identification of Potential drug targets for Enterococcus
+                    sp. using in silico tools.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Analysis of commercially available cancer drug against EGFR
+                    in case of oral cancer.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Drug designing for control of infection of Heliobacter
+                    pylori in Humans.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Identification of Biomarkers in case of cervical or prostate
+                    cancer using Bioinformatic tools.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    In silico Analysis of Inhibitors of ACE receptor with
+                    respect to COVID Treatment.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Understanding the Structures and activity of SARS CoV E
+                    receptor protein.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    In silico analysis of drugs against Hemolysin protein
+                    produced due to bacterial infections.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    A review on the Nucleotide and Proteomic In silico data on
+                    SARS CoV using different softwares & tools.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    A comparative study of Protein Prediction online tools and
+                    their importance in drug designing.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    An in silico analysis on the efficacy of Potential
+                    inhibitors for SAR CoV already available.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    A review of different bioinformatic tools being used to
+                    study cancer treatment.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Chikungunya - An in silico analysis of the structural & Non-
+                    structural proteins of the virus.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <br />
+            <br />
+            {/*end of row2*/}
+
+            <div className="row pt-3 pb-5">
+              <div className="col-lg-8 " style={{ textAlign: "left" }}>
+                <h6>
+                  List of Topics for IPR (Intellectual property Rights) Related
+                  Projects
+                </h6>
+                <ul>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Biotechnology patenting, difference between USA and Indian
+                    Perspective.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    The role of IPR in protection of Biodiversity in Indian
+                    context.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Pharmaceutical patenting in India-problem of public access
+                    to health.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    The effectiveness of the remedies for copyright
+                    infringement, a global concern.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Traditional knowledge and patent issues: with reference to
+                    Basmati, Neem, Turmeric, and Golden rice.
+                  </li>
+                  <li>
+                    <i
+                      class="far fa-dot-circle"
+                      style={{ color: "#5dd39e", padding: "5px" }}></i>
+                    Biotechnology patenting and bio-diversity protection:
+                    resolving conflict.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Bcu; 
+export default Bcu;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,94 +1,126 @@
-import React from 'react';
-import {BrowserRouter as Router,Link,NavLink} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Link, NavLink } from "react-router-dom";
 import "../assets/styles/nav.css";
-import logo from "../assets/images/logo.png"
+import logo from "../assets/images/logo.png";
 class Nav extends React.Component {
-  render() {    
+  render() {
     return (
-        <div className="navigation">
+      <div className="navigation">
         <section class="header-uper ">
-            <div class="container-fluid clearfix">
-                    <div>
-                    <a class="navbar-brand" href="#" target="_self">
-                        <img src={logo} width="auto" height="80"/>
-                    </a>
-                    </div>
-                    <div class="float-right">
-                        <div className="social-links phone-number">
-                            <span className="d-flex phone-number">
-                                <i className="fas fa-phone-alt"></i>
-                                <p>9811668417 / 0120-4525825</p>
-                            </span>
-                        </div>
-                        <ul class="social-links">
-                                <li class="item">
-                                    <div>
-                                        <i class="fa fa-facebook" aria-hidden="true"></i>
-                                    </div>
-                                </li>
-                                <li class="item">
-                                    <div>
-                                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                                    </div>
-                                </li>
-                                <li class="item">
-                                    <div>
-                                    <i class="fa fa-google-plus" aria-hidden="true"></i>
-                                    </div>
-                                </li>
-                        </ul>
-                    </div>
+          <div class="container-fluid clearfix">
+            <div>
+              <a class="navbar-brand" href="#" target="_self">
+                <img src={logo} width="auto" height="80" />
+              </a>
             </div>
+            <div class="float-right">
+              <div className="social-links phone-number">
+                <span className="d-flex phone-number">
+                  <i className="fas fa-phone-alt"></i>
+                  <p>9811668417 / 0120-4525825</p>
+                </span>
+              </div>
+              <ul class="social-links">
+                <li class="item">
+                  <div>
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                  </div>
+                </li>
+                <li class="item">
+                  <div>
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                  </div>
+                </li>
+                <li class="item">
+                  <div>
+                    <i class="fa fa-google-plus" aria-hidden="true"></i>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
         </section>
         <nav className="navbar navbar-expand-lg navbar-dark navbar-custom">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                <li className="nav-item">
-                <NavLink className={`nav-link`} 
-                    activeClassName="active"
-                    exact to="/">Home</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className={`nav-link`} 
-                    activeClassName="active"
-                    to="/aboutus">About Us</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className={`nav-link `} 
-                    activeClassName="active"
-                    to="/biotech-rnd">R&D</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className={`nav-link `} 
-                    activeClassName="active"
-                    to="/biotech-tk">Manufacturing Kits</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className={`nav-link `} 
-                    activeClassName="active"
-                    to="/tpd">Services</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className={`nav-link `} 
-                    activeClassName="active"
-                    to="/quality-testing">Quality Testing</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className={`nav-link `} 
-                    activeClassName="active"
-                    to="/bio-com-units">Bio Composting Units</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className={`nav-link `} 
-                    activeClassName="active"
-                    to="/contact">Contact Us</NavLink>
-                </li>
-                </ul>
-            </div>
-      </nav>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link`}
+                  activeClassName="active"
+                  exact
+                  to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link`}
+                  activeClassName="active"
+                  to="/aboutus">
+                  About Us
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link `}
+                  activeClassName="active"
+                  to="/biotech-rnd">
+                  R&D
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link `}
+                  activeClassName="active"
+                  to="/biotech-tk">
+                  Manufacturing Kits
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link `}
+                  activeClassName="active"
+                  to="/tpd">
+                  Services
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link `}
+                  activeClassName="active"
+                  to="/quality-testing">
+                  Quality Testing
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link `}
+                  activeClassName="active"
+                  to="/bio-com-units">
+                  Projects for Training
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className={`nav-link `}
+                  activeClassName="active"
+                  to="/contact">
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
